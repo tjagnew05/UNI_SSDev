1417348a7fcc332700972cbf8dd246cfdd4d7481
Thomas Agnew <thomas@Thomass-Mac-mini.local>
2026-02-23 21:03:47 +0000

    Initial commit

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,4 +9,10 @@ class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+    public function tags()
+    {
+      return $this->belongsToMany(Tag::class, relatedPivotKey: "job_listing_id");
+    }
+  
 }
